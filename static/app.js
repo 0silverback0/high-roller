e9f1f21c20108c70b973f8531a55e8b0d4b3c839
me <fourmarz@yahoo.com>
2022-12-01 00:19:42 -0500

    UI update

--- static/app.js
+++ static/app.js
@@ -1,8 +1,21 @@
-let $modalBtn = $('.modalbtn')
-
-let $filter = $('.modal')
+const $modalBtn = $('.modalbtn')
+const $filter = $('.modal')
+const $addTocart = $('.add-to-cart')
+const $cart = $('.test')
 
 $modalBtn.click( e => {
     $filter.modal('show')
     console.log($modalBtn)
+})
+
+$addTocart.click( e => {
+    e.preventDefault()
+    // alert('here')
+    $cart.addClass('cart')
+    
+    setTimeout(() => {
+        $cart.removeClass('cart')
+        console.log('here')
+    }, 1100);
+
 })
